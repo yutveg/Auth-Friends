37482f74bb6ee4d4d10e1a6813c004694baab2fc
yutveg <kodi.0n31l@gmail.com>
2019-12-17 16:27:53 -0800

    delete function works

--- friends-app/src/components/Friend.js
+++ friends-app/src/components/Friend.js
@@ -1,10 +1,21 @@
 import React from "react";
+import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Friend = props => {
+  const deleteFriend = () => {
+    console.log(props.friend.id);
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/friends/${props.friend.id}`)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="friend">
       <h1>{props.friend.name}</h1>
       <h2>{props.friend.age}</h2>
+      <button onClick={deleteFriend}>X</button>
     </div>
   );
 };
